e9f4cb7c4b1993e173c093057a7d79c69c421ef3
Or Ashry <orashry@gmail.com>
2024-07-16 17:37:54 +0300

    fix define name

--- FirstPass.h
+++ FirstPass.h
@@ -1,10 +1,10 @@
 include <stdio.h>
-define SYMBOL_NAME_MAX 32
+define MAX_SYMBOL_NAME 32
 
 struct symbol {
     char symbol_name[SYMBOL_NAME_MAX];
     int address;
-    
+
     enum {
         extern_symbol;
         entry_symbol;
