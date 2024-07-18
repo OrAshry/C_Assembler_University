469fb00f019672ee66458f62020a68b184b3e221
Or Ashry <orashry@gmail.com>
2024-07-18 20:58:32 +0300

    added prototypes

--- SymbolTable.h
+++ SymbolTable.h
@@ -4,6 +4,10 @@
 #define MAX_LINE_LENGTH 81
 #define MAX_SYMBOL_NAME 32
 
+/* Prototypes */
+void add_symbol_to_table(symbol new);
+int symbol_search(table_ptr p, const char search_name[MAX_SYMBOL_NAME]);
+
 enum type{
     extern_symbol,
     entry_symbol,
