76f397ea1687afda1fead8920dca88b18093f816
Or Ashry <orashry@gmail.com>
2024-07-26 18:20:20 +0300

    removing symbol struct, I only needs the table

--- SymbolTable.h
+++ SymbolTable.h
@@ -6,7 +6,7 @@
 #define MAX_SYMBOL_NAME 32
 
 /* Prototypes */
-void add_symbol_to_table(symbol new);
+void add_symbol_to_table();
 int symbol_search(table_ptr p, const char search_name[MAX_SYMBOL_NAME]);
 
 enum type{
@@ -18,13 +18,9 @@ enum type{
     entry_data
 };
 
-typedef struct symbol {
+typedef struct symbol_table {
     char symbol_name[MAX_SYMBOL_NAME];
     enum type symbol_type;
-} symbol, * symbol_ptr;
-
-typedef struct symbol_table {
-    symbol;
     int address;
     struct symbol_table *next;
 } symbol_table, * table_ptr;
