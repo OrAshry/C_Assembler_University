bf9313c1639f87d708361eab01edb2e779dc602e
Or Ashry <orashry@gmail.com>
2024-07-26 19:05:00 +0300

    add symbol function draft

--- AddSymbol.c
+++ AddSymbol.c
@@ -1,5 +1,20 @@
 #include "SymbolTable.h"
 
-void add_symbol_to_table(symbol name) {
-    ;
+void add_symbol_to_table(char new_name[MAX_SYMBOL_NAME], enum type new_type, int new_address, table_ptr ptr) {
+    
+    table_ptr new_symbol = (table_ptr)allocateMemory(1, sizeof(symbol_table), CALLOC_ID);
+    strcpy(new_symbol -> symbol_name, new_name);
+    new_symbol -> symbol_type = new_type;
+    new_symbol -> symbol_address = new_address;
+    new_symbol-> next = NULL;
+    /* The table is empty */
+    if(ptr == NULL) {
+        ptr = new_symbol;
+    }
+    /* The table is not empty */
+    else {
+        table_ptr current = ptr;
+        ptr = new_symbol;
+        current -> next = ptr;
+    }
 }
