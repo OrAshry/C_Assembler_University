cd9b725a3760a2cf0c23a647709da1f800938286
Or Ashry <orashry@gmail.com>
2024-07-26 21:54:14 +0300

    making a base structure for firstPass function

--- FirstPass.c
+++ FirstPass.c
@@ -1,6 +1,7 @@
 #include "FirstPass.h"
 
-int firstPass(char * file_name, FILE * file, table_ptr p) {
+int firstPass(char * file_name, FILE * file) {
+    
     /* Declarations */
     int error_flag = 0;
     int IC = 0; /* need to check if its starts from 0 or 100*/
@@ -9,39 +10,49 @@ int firstPass(char * file_name, FILE * file, table_ptr p) {
     int line_counter = 0; /* The number of line i just read from (am file) */
     char read_line[MAX_LINE_LENGTH];
     struct ast answer = {0}; /* After front returned answer*/
-    symbol_ptr p1;
+    table_ptr p1; /* The poiner to the symbol table*/
 
     /* Read lines from the am file */
     while(fgets(read_line,sizeof(read_line),am_file)) {
         answer = front_answer(read_line);
         
         /* If there is a syntax error*/
-        if(line.lineError[0] != '\0') {
-            printf("In file %s at line %d there is a sysntax error: %s", am_file,line_counter,line.lineError);
+        if(answer.lineError[0] != '\0') {
+            printf("Error: In file %s at line %d there is a sysntax error: %s\n", am_file,line_counter,answer.lineError);
             line_counter++;
             error_flag = 1;
             continue;
         }
 
         /* If there is a symbol in the line */
-        if((line.symbol == 1) && ((line.type == data) || line.type == string)) {
+        if((answer.labelName != null) && ((answer.ast_type == data) || answer.ast_type == string)) {
             
-            /* If the symbol is already exist in the table*/
-            if(symbol_search()) {
+            /* If the symbol is already exist in the table */
+            if(symbol_search(p1, answer.labelName)) {
                 
+                /* If entry*/
+                if(answer.ast_type == entry) {
+                    ;
+                }
+                
+                /* If its not an entry*/
+                else {
+                    printf("Error: In file %s at line %d the symbol %s has been redefined.\n", am_file,line_counter, answer.labelName);
+                    line_counter++;
+                    error_flag = 1;
+                }
             }
 
             /* If the symbol is not in the table*/
             else {
-                add_symbol_to_table();
+                
+                /* If its IC */
+                add_symbol_to_table(answer.labelName, answer.ast_type, IC, p1);
+
+                /* If its DC*/
+                add_symbol_to_table(answer.labelName, answer.ast_type, DC, p1);
             }
         }
-
-        
-
-
-
-
 
     }
 
