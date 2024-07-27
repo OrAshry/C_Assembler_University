a0e4d72dfb22e800f2bdd5f86b1722d89076ea6d
Or Ashry <orashry@gmail.com>
2024-07-27 21:29:29 +0300

    finished symbol table

--- FirstPass.c
+++ FirstPass.c
@@ -7,7 +7,7 @@ int firstPass(char * file_name, FILE * file) {
     int IC = 0; /* need to check if its starts from 0 or 100*/
     int DC = 0; /* need to check if it starts from 0 or 100*/
     int L = 0; /* Number of words that the current instruction takes */
-    int line_counter = 0; /* The number of line i just read from (am file) */
+    int line_counter = 1; /* The number of line i just read from (am file) */
     char read_line[MAX_LINE_LENGTH];
     struct ast answer = {0}; /* After front returned answer*/
     table_ptr p1; /* The poiner to the symbol table*/
@@ -26,27 +26,37 @@ int firstPass(char * file_name, FILE * file) {
         }
 
         /* If there is a symbol in the line */
-        if((answer.labelName != '\0') && ((answer.ast_type == ast_inst) || answer.ast_type == ast_dir)) {
+        if((answer.labelName != NULL) && ((answer.ast_type == ast_inst) || answer.ast_type == ast_dir)) {
             
             /* If the symbol is already exist in the table */
             if(found = symbol_search(p1, answer.labelName)) {
                 
-                /* If entry*/
+                /* If the symbol in the table is entry*/
                 if(found -> symbol_type == entry_symbol) {
                     
-                    /* If Inst */
-                    if(answer.dir_type ) {
-                        found -> 
+                    /* If the symbol in the line is inst */
+                    if(answer.ast_type == ast_inst) {
+                        found -> symbol_type = entry_code;
                     }
-
-                    /* If Dir */
-
-                }
+                    
+                    /* If the symbol in the line is dir */
+                    else if(answer.ast_type == ast_dir) {
+                        
+                        /* If its data or string */
+                        if(answer.ast_options.dir.dir_type == ast_data || ast_string) {
+                            found -> symbol_type = entry_data;
+                        }
+                        
+                        /* If its entry or extern */
+                        else if(answer.ast_options.dir.dir_type == ast_entry || ast_extern) {
+                            printf("Error: In file %s at line %d the symbol %s has been redefined.\n", am_file,line_counter, answer.labelName);
+                            error_flag = 1;
+                        }
+                    }
                 
-                /* If its not an entry*/
+                /* If the symbol in the table is not entry*/
                 else {
                     printf("Error: In file %s at line %d the symbol %s has been redefined.\n", am_file,line_counter, answer.labelName);
-                    line_counter++;
                     error_flag = 1;
                 }
             }
