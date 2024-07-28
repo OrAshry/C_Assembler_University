7685e276b7f4612ee39e887dfd1fc0e8ed935cfd
Or Ashry <orashry@gmail.com>
2024-07-28 14:14:12 +0300

    corected the use of the enum, it neded casting

--- AddSymbol.c
+++ AddSymbol.c
@@ -4,13 +4,15 @@ void add_symbol_to_table(char new_name[MAX_SYMBOL_NAME], int new_type, int new_a
     
     table_ptr new_symbol = (table_ptr)allocateMemory(1, sizeof(symbol_table), CALLOC_ID);
     strcpy(new_symbol -> symbol_name, new_name);
-    new_symbol -> symbol_type = new_type;
+    new_symbol -> symbol_type = (enum type)new_type;
     new_symbol -> symbol_address = new_address;
     new_symbol-> next = NULL;
+    
     /* The table is empty */
     if(*ptr == NULL) {
         *ptr = new_symbol;
     }
+    
     /* The table is not empty */
     else {
         table_ptr current = *ptr;
