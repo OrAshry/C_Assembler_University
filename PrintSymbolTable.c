847585c970024bc68898b9f01e041c5af1e0aff2
Or Ashry <orashry@gmail.com>
2024-07-30 19:47:51 +0300

    added the next coulmn in the print func

--- PrintSymbolTable.c
+++ PrintSymbolTable.c
@@ -4,8 +4,8 @@
 void print_symbol_table(table_ptr p) {
     printf("Symbol Table:\n");
     while (p != NULL) {
-        printf("Name: %-15s Type: %-15d Address: %-5d\n",
-               p->symbol_name, p->symbol_type, p->symbol_address);
+        printf("Name: %-15s Type: %-15d Address: %-5d Next : 0d\n",
+               p->symbol_name, p->symbol_type, p->symbol_address, p->next);
         p = p->next;
     }
 }
