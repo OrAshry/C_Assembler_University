2944c59fe356f24b7549faf70eece9205bc4896a
Or Ashry <orashry@gmail.com>
2024-07-30 20:00:08 +0300

    casting void pointer instead of a struct symbol_table pointer, becuase thats what the printf format required

--- PrintSymbolTable.c
+++ PrintSymbolTable.c
@@ -5,7 +5,7 @@ void print_symbol_table(table_ptr p) {
     printf("Symbol Table:\n");
     while (p != NULL) {
         printf("Name: %-15s Type: %-15d Address: %-5d Next : %p\n",
-               p->symbol_name, p->symbol_type, p->symbol_address, p->next);
+               p->symbol_name, p->symbol_type, p->symbol_address, (void *)p->next);
         p = p->next;
     }
 }
