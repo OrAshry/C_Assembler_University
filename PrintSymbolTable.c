c7c3a71c91223911a9111933cf48324f392f7c6e
Or Ashry <orashry@gmail.com>
2024-07-30 19:51:16 +0300

    fix the type of print for the pointer(it was int)

--- PrintSymbolTable.c
+++ PrintSymbolTable.c
@@ -4,7 +4,7 @@
 void print_symbol_table(table_ptr p) {
     printf("Symbol Table:\n");
     while (p != NULL) {
-        printf("Name: %-15s Type: %-15d Address: %-5d Next : 0d\n",
+        printf("Name: %-15s Type: %-15d Address: %-5d Next : %p\n",
                p->symbol_name, p->symbol_type, p->symbol_address, p->next);
         p = p->next;
     }
