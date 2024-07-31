fa38580ca4897c73d13ab3ccc3cd6716aac35028
Tomer Shein <tomersheints@gmail.com>
2024-07-31 15:59:01 +0300

    finished with parse_operand function

--- front.h
+++ front.h
@@ -58,7 +58,7 @@ struct inst inst_table[INST_SIZE] = {
 
 struct ast{
     char lineError[ERROR_LINE];
-    char * labelName; 
+    char labelName[MAX_LABEL_SIZE]; 
     enum{
         ast_inst,
         ast_dir,
@@ -76,7 +76,7 @@ struct ast{
                 ast_data
             }dir_type;
             struct{
-                char * label;
+                char label[MAX_LABEL_SIZE];
                 int data_size;
                 int data[RESULT_ARR_SIZE];
             }dir_options;
@@ -93,15 +93,15 @@ struct ast{
             }inst_type;
             struct{
                 enum{
-                    ast_none,
                     ast_immidiate,
                     ast_label,      
+                    ast_register_address,
                     ast_register_direct,
-                    ast_register_address
+                    ast_none
                 }operand_type;
                 union{
                     int immed;
-                    char * label;
+                    char label[MAX_LABEL_SIZE];
                     int reg;
                 }operand_option;
             }operands[2];
@@ -111,13 +111,15 @@ struct ast{
 };
 
 struct ast get_ast_from_line(char * line);
-static int is_number(char * str, int min_num, int max_num, int * result, char ** end_ptr);
-static int validate_numbers(struct string_split split_str, int size, struct ast *ast, int index);
-static int is_label(char *str, struct ast *ast);
-static int is_register(char *str);
-static int is_instruction(char *str, struct ast * ast);
-static void parse_operand(char * operand, int operand_type ,struct ast * ast, struct inst inst);
-static void parse_operands(struct string_split operands, int index, struct ast * ast);
+int is_number(char * str, int min_num, int max_num, int * result, char ** end_ptr);
+int validate_numbers(struct string_split split_str, int size, struct ast *ast, int index);
+int is_label(char const *str, struct ast *ast);
+int is_register(char const *str);
+int is_instruction(char const *str, struct ast * ast);
+int is_op_valid(int const operand_type, char const * inst_options);
+void parse_operand(char * operand, int operand_type ,struct ast * ast, int operand_index);
+void parse_operands(struct string_split operands, int index, struct ast * ast);
 struct string_split split_string(char * str, const char * delimiter);
-static int fill_string(struct string_split split_result, int index, struct ast *ast);
-static void fill_directive_ast(struct ast * ast, struct string_split split_result, int index);
+int fill_string(struct string_split split_result, int index, struct ast *ast);
+void fill_directive_ast(struct ast * ast, struct string_split split_result, int index);
+int validate_numbers(struct string_split const split_str, int const size, struct ast *ast, int const index);
