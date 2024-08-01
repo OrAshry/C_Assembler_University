0ead71172e1982bf355f3c7bf11a8eb00865d184
Or Ashry <orashry@gmail.com>
2024-08-01 21:06:21 +0300

    added print function for data_image

--- PrintSymbolTable.c
+++ PrintSymbolTable.c
@@ -1,5 +1,6 @@
 /*for the test*/
 #include "SymbolTable.h"
+#include "FirstPass.h"
 
 void print_symbol_table(table_ptr p) {
     printf("Symbol Table:\n");
@@ -7,5 +8,14 @@ void print_symbol_table(table_ptr p) {
         printf("Name: %-15s Type: %-15d Address: %-5d Next : %p\n",
                p->symbol_name, p->symbol_type, p->symbol_address, (void *)p->next);
         p = p->next;
+    }
+}
+
+void print_data_image(const translate *ptr_to_translate) {
+    printf("Data Image:\n");
+    for (int i = 0; i < MAX_MEMORY_SIZE; i++) {
+        if (ptr_to_translate->data_image[i] != 0) {  // Print only non-zero values
+            printf("Address %d: %d\n", i, ptr_to_translate->data_image[i]);
+        }
     }
 }
