f0f0bb09ae0ea0b6e8024f6650fcf420b808bf8e
Or Ashry <orashry@gmail.com>
2024-08-03 16:06:03 +0300

    fixed syntax errors

--- SecondPass.c
+++ SecondPass.c
@@ -4,7 +4,6 @@ int secondPass(char * file_name, FILE * file) {
     
     /* Declarations */
     int error_flag = 0;
-    int source_line_counter = 1;
     char line[MAX_LINE_LENGTH]; /* The line muber of the source file after macro */
     struct ast answer_line = {0};
     int L; /* Words counter*/
@@ -16,14 +15,14 @@ int secondPass(char * file_name, FILE * file) {
 
         /* Calculate words and code them into data_image*/
         if((answer_line.ast_type == ast_dir) && ((answer_line.ast_options.dir.dir_type == ast_data) || (answer_line.ast_options.dir.dir_type == ast_string))) {
-            memcpy(machine_code_ptr -> data_image[machine_code_ptr -> DC], answer_line.ast_options.dir.dir_options.data, answer_line.ast_options.dir.dir_options.data_size);
+            memcpy(&machine_code_ptr -> data_image[machine_code_ptr -> DC], answer_line.ast_options.dir.dir_options.data, answer_line.ast_options.dir.dir_options.data_size);
             L = answer_line.ast_options.dir.dir_options.data_size;
             (machine_code_ptr -> DC) += L;
 
         }
 
         /* Calculate words if its inst*/
-        if(answer_line.ast_type = ast_inst) {
+        if((answer_line.ast_type = ast_inst)) {
             for (i = 0; i < 2; i++) {
                 if (answer_line.ast_options.inst.operands[i].operand_type != ast_none) {
                     L++;
