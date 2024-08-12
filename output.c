6f1e948587a493512ca81c2b6cf01fa2c647fb7e
Or Ashry <orashry@gmail.com>
2024-08-12 20:16:47 +0300

    finished the function that creates entry file

--- output.c
+++ output.c
@@ -1,21 +1,37 @@
 #include "output.h"
 
-void createEntFile(const char *source_file_name) {
+/* Entry's output file */
+void createEntFile(const char *input_file_name) {
     char *ent_file_name;
-    FILE * file;
-    int count;
-    found = head_ptr;
-    char *symbol_name;
-    int i;
+    FILE *ent_file;
+    table_ptr find = head_ptr;
 
-        found = find_entry(head_ptr);
-        if(found) {
-            count++;
-            if(count == 1) {
-                
-            }
-        }
-    
+    /* Check if there are entry symbols */
+    if(find_entry(find) == NULL) {
+        return;
+    }
 
-    fclose(source_file_name);
+    /* allocate memory for the .ent file */
+    ent_file_name = (char *)allocateMemory(1, strlen(input_file_name) + 5, MALLOC_ID);
+
+    /* Create the .ent file name */
+    strcpy(ent_file_name, input_file_name);
+    strcat(ent_file_name, ".ent");
+
+    /* Open .ent file for writing */
+    ent_file = fopen("ent_file_name", "w");
+    if(!ent_file) {
+        fprintf(stderr, "Could not open the file %s for writing\n", ent_file_name);
+        free(ent_file);
+        return;
+    }
+
+    /* Writing the symbol names and addresses*/
+    while((find = find_entry(find)) != NULL) {
+        fprintf(ent_file, "%s %d\n", find -> symbol_name, find -> symbol_address);
+        find = find -> next;
+    }
+
+    fclose(ent_file);
+    free(ent_file_name);
 }
