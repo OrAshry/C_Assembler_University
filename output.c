1bc5413cca4c72dde27fcff682bbbed8e2d2c187
Or Ashry <orashry@gmail.com>
2024-08-16 17:02:42 +0300

    continue with output.h

--- output.c
+++ output.c
@@ -91,6 +91,7 @@ void createExtFile(const char *input_file_name)
 void createObFile(const char *input_file_name) {
     char *ob_file_name;
     FILE *ob_file;
+    int i;
 
     /* Check if there is any code  */
     if((machine_code_ptr->DC == 0) && (machine_code_ptr->IC == 0))
@@ -116,6 +117,9 @@ void createObFile(const char *input_file_name) {
 
     /* Writing the header (number of instructions and number of directive) */
     fprintf(ob_file, "%d\t%d\n", (machine_code_ptr->IC) - 100,  (machine_code_ptr->DC));
+
+    /* Writing the code_image */
+    fprint_code_image(machine_code_ptr, ob_file);
     
     /* Clean up */
     fclose(ob_file);
